1e4ba330b0abcee35b9cc29de76e3f93db3543d5
ashmfran <143596924+ashmfran@users.noreply.github.com>
2025-01-27 22:24:50 +0000

    Testing Mon Jan 27 2025 22:24:50 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/cube.py
+++ chapter2/ex02/student/cube.py
@@ -1,1 +1,18 @@
-# Write your program here
+def cube_surface_area(side_length):
+  """Calculates the surface area of a cube.
+
+  Args:
+    side_length: The length of a side of the cube.
+
+  Returns:
+    The surface area of the cube.
+  """
+
+  return 6 * side_length ** 2
+
+# Get the side length from the user
+side_length = float(input("Enter the side length of the cube: "))
+
+# Calculate and print the surface area
+surface_area = cube_surface_area(side_length)
+print("The surface area of the cube is:", surface_area)# Write your program here
