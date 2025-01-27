6d59a6c2fdb0b21dd4bad3ae48479cf7dcebfaba
ashmfran <143596924+ashmfran@users.noreply.github.com>
2025-01-27 17:50:04 +0000

    Testing Mon Jan 27 2025 17:50:04 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex01/student/myinfo.py
+++ chapter1/ex01/student/myinfo.py
@@ -1,3 +1,7 @@
 name = "Ashley Francis"
 address = "2590 Welton Street, Apt 420 Denver CO, 80205"
 telephone_number = "813 892 0899"
+
+print ("Name: ", name)
+print ("Address: ", address)
+print ("Telephone Number: ", telephone_number)
