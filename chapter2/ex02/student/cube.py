dcd7e2b5ad66e28728cf7caf3dd59bdd3e73a822
ashmfran <143596924+ashmfran@users.noreply.github.com>
2025-01-27 22:15:10 +0000

    Testing Mon Jan 27 2025 22:15:10 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/cube.py
+++ chapter2/ex02/student/cube.py
@@ -1,1 +1,13 @@
-# Write your program here
+"""
+Author: Ashley Francis
+Date written: 01/27/2025
+Assignment: Module 01 Practice Exercise 2-2
+This program defines a function to calculate the surface area of a cube
+using the formula 6 * side^2; the side is the length of one edge of the cube.
+The program will prompt the user to input the length of one edge of the cube,
+it will then calculate the surface area. The program will display the surface area.
+"""
+edge_length = int(input("Enter the length of an edge of the cube: "))
+
+surface_area = 6 * (edge_length ** 2)
+print(f"The surface area of the cube is: {surface_area}")# Write your program here
