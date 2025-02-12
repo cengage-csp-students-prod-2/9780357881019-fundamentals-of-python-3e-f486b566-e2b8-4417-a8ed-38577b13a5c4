839e28a28f204d7f549124e548c9128ab22c3f55
ashmfran <143596924+ashmfran@users.noreply.github.com>
2025-02-12 02:59:30 +0000

    Testing Wed Feb 12 2025 02:59:30 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex08/student/concordance.py
+++ chapter5/ex08/student/concordance.py
@@ -1,1 +1,29 @@
-# Write your program here
+"""
+Author: Ashley Francis
+Date written: 02/11/2025
+Assignment: Module 04 Practice Exercise 5-8
+This programs is designed to output the unique words and
+their frequencies in alphabetical order.
+"""
+
+inName = input("Enter the input file name: ")
+
+inputFile = open(inName, 'r')
+uniqueWords = {}
+
+for line in inputFile:
+    words = line.split()
+
+    for word in words:
+        if word in uniqueWords:
+            uniqueWords[word] = uniqueWords[word] + 1
+        else:
+            uniqueWords[word] = 1
+
+inputFile.close()
+
+wordList = list(uniqueWords.keys())
+wordList.sort()
+
+for word in wordList:
+    print(word, uniqueWords[word])# Write your program here
